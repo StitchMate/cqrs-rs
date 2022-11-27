780d1b0852b02333760f13cd6727badd6a2fc6fd
cawfeecoder <nick@frushco.de>
2022-11-27 16:53:53 -0500

    type update

--- src/application/port/outbound/event_bus.rs
+++ src/application/port/outbound/event_bus.rs
@@ -5,7 +5,7 @@ use futures::Stream;
 pub trait EventBus<IE, T, OE>
 where
     IE: Into<T>,
-    OE: From<T>,
+    T: Into<OE>,
 {
     async fn send_event(&self, event: IE) -> Result<(), anyhow::Error>;
     async fn receive_events(&self) -> Result<Box<dyn Stream<Item = OE>>, anyhow::Error>;
